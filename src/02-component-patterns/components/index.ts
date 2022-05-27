716bd875cc491aba34e49bd5f798fe34f247f5fe
Facundo Tanovich <facundotanovich@gmail.com>
2022-05-27 18:40:00 -0300

    Component compound listo

---
+++ src/02-component-patterns/components/index.ts
@@ -0,0 +1,20 @@
+import { ProductCardHCProps } from '../interfaces/interfaces';
+
+import { ProductCard as ProductCardHOC } from './ProductCard';
+
+import { ProductButtons } from './ProductButtons';
+import { ProductImage } from './ProductImage';
+import { ProductTitle } from './ProductTitle';
+
+export { ProductButtons } from './ProductButtons';
+export { ProductImage } from './ProductImage';
+export { ProductTitle } from './ProductTitle';
+
+
+export const ProductCard: ProductCardHCProps = Object.assign( ProductCardHOC, {
+    Title: ProductTitle,
+    Image: ProductImage,
+    Buttons: ProductButtons
+})
+
+export default ProductCard;
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate } from 'react-router-dom';
 import { Routes, Route, NavLink } from 'react-router-dom';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 import logo from '../logo.svg'
 
@@ -11,7 +12,7 @@ export const Navigation = () => {
                 <img src={ logo } alt="React logo" />
                 <ul>
                     <li>
-                        <NavLink to="/home" className={ ( {isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                        <NavLink to="/shopping" className={ ( {isActive }) => isActive ? 'nav-active' : '' }>Shopping</NavLink>
                     </li>
                     <li>
                         <NavLink to="/about" className={ ( {isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -25,7 +26,8 @@ export const Navigation = () => {
             <Routes>
                 <Route path="about" element={ <h1>About Page</h1> } />
                 <Route path="users" element={<h1>Users Page</h1>} />
-                <Route path="home" element={<h1>Home Page</h1>} />
+                <Route path="shopping" element={<ShoppingPage />} />
+
                 <Route path="/*" element={<Navigate to="/home" replace />} />
             </Routes>
         </div>
