35aefe658c7996308034176f1d8dd5022ef03842
Facundo Tanovich <facundotanovich@gmail.com>
2022-06-07 15:28:18 -0300

    Control props alternativa finalizada

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,34 +8,20 @@ export const useShoppingCart = () => {
         // console.log(count, product);
 
         setShoppingCart( oldShoppingCart => {
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
-
-            if( Math.max( productInCart.count + count, 0 ) > 0 ){
-                productInCart.count += count;
-                return{
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
-            }
-
-            //Borrar producto
-            const { [product.id]: toDelte, ...rest } = oldShoppingCart;
-            return rest;
-
             
             // if count is 0, remove product from cart
-            // if (count === 0) {
-            //     const { [product.id]: toDelte, ...rest } = oldShoppingCart;
-            //     return rest;
-            // }
+            if (count === 0) {
+                const { [product.id]: toDelte, ...rest } = oldShoppingCart;
+                return rest;
+            }
 
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id]: {
-            //         ...product,
-            //         count
-            //     }
-            // }
+            return {
+                ...oldShoppingCart,
+                [product.id]: {
+                    ...product,
+                    count
+                }
+            }
         })
     }
     
